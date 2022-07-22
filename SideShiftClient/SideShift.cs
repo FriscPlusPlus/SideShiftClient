ce839e5887e882303412c5d13d9bd60e7acca2e6
Firas Jelassi <49401280+Friscas@users.noreply.github.com>
2022-07-22 11:27:57 +0200

    first commit

---
+++ SideShiftClient/SideShift.cs
@@ -0,0 +1,20 @@
+﻿using Httwrap.Interface;
+using Httwrap;
+
+namespace SideShiftClient
+{
+    public class SideShift
+    {
+        IHttwrapConfiguration? configuration = null;
+        IHttwrapClient? client = null;
+        public SideShift()
+        {
+            this.configuration = new HttwrapConfiguration("https://sideshift.ai/api/v2/");
+            this.client = new HttwrapClient(this.configuration);
+        }
+        public void Test()
+        {
+            Console.WriteLine("HELLOW WORLD!");
+        }
+    }
+}
