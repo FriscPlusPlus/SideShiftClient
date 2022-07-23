36fb038254e1f586455627ee411b8b71e14b0121
Firas Jelassi <49401280+Friscas@users.noreply.github.com>
2022-07-23 20:15:27 +0200

    added method to parse arrays, and methods for coins and permissions

--- SideShiftClient/SideShift.cs
+++ SideShiftClient/SideShift.cs
@@ -1,38 +1,54 @@
 ﻿using Httwrap.Interface;
 using Httwrap;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Reflection;
 
 namespace SideShiftClient
 {
     public class SideShift
     {
-        IHttwrapConfiguration? configuration = null;
-        IHttwrapClient? client = null;
-        string? sideShiftSecret = null;
+        private static IHttwrapConfiguration configuration = new HttwrapConfiguration("https://sideshift.ai/api/v2/");
+        private static IHttwrapClient client = new HttwrapClient(configuration);
+        private string? sideShiftSecret = null;
         public SideShift([Optional] string sideShiftSecret)
         {
-            this.configuration = new HttwrapConfiguration("https://sideshift.ai/api/v2/");
-            this.client = new HttwrapClient(this.configuration);
             if (sideShiftSecret != null)
             {
                 this.sideShiftSecret = sideShiftSecret;
             }
         }
-        public void GetCoins() { }
-        public void GetPermissions() { }
-        public void GetPair() { }
-        public void GetPairs() { }
-        public void GetBulkShifts() { }
-        public void RequestQuote() { }
-        public void CreateFixedShift() { }
-        public void CreateVariableShift() { }
-        public void GetShift() { }
-        public void SetRefundAddress() { }
-        public void GetXAIStats() { }
-        public void GetAccount() { }
-        public void ListRecentShifts() { }
+        public static async Task<List<dynamic>> GetCoins()
+        {
+            var res = await client.GetAsync("coins");
+            var parsedData = ParseArrayData(res.Body);
+            return parsedData;
+        }
+        public static async Task<bool> RequestsAllowed() {
+            var res = await client.GetAsync("permissions");
+            var parsedData = JsonConvert.DeserializeObject<Permissions>(res.Body);
+            return parsedData.createShift;
+        }
+        /*  
+         public JObject GetPair() { }
+         public JObject GetPairs() { }
+         public JObject GetBulkShifts() { }
+         public JObject RequestQuote() { }
+         public JObject CreateFixedShift() { }
+         public JObject CreateVariableShift() { }
+         public JObject GetShift() { }
+         public JObject SetRefundAddress() { }
+         public JObject GetXAIStats() { }
+         public JObject GetAccount() { }
+         public JObject ListRecentShifts() { }*/
 
-        private Boolean CanDoRequest()
+        private static List<dynamic> ParseArrayData(string responseBody)
+        {
+            var parsedData = JsonConvert.DeserializeObject<List<dynamic>>(responseBody);
+            return parsedData;
+        }
+
+        private bool CanDoRequest()
         {
             return true;
         }
---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿using SideShiftClient;
+class Program
+{
+    static async Task Main(string[] args) {
+        SideShift shift = new SideShift();
+        var data = await SideShift.RequestsAllowed();
+        Console.WriteLine(data);
+    }
+}
